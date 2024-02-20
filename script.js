74ff55fb278a1c0cc51284caf7e91f52308f8bd0
Parag-Chaware <148242322+Parag-Chaware@users.noreply.github.com>
2024-02-20 21:33:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function getFormvalue() {
     //Write your code here
+	let name = document.querySelectorAll('input');
+	let firstName = name[0].value;
+	let lastName = name[1].value;
+	alert(firstName + ' ' + lastName);
 
 }
